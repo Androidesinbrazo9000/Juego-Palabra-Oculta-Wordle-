046daea1ad6cfc70599b9ef89b88905c4126ca01
CristinaB93 <cberrocalm93@gmail.com>
2026-05-11 19:41:13 +0200

    A ver si se sube el .txt

--- Wordle/src/JuegoWordle/Partida.java
+++ Wordle/src/JuegoWordle/Partida.java
@@ -2,6 +2,13 @@ package JuegoWordle;
 
 public class Partida {
 	
+	private final String RUTA = ""
 	private Hashset<String> palabras = new Hashset<>();
+	
+	public extraerPalabras() {
+		
+		
+		
+	}
 
 }
