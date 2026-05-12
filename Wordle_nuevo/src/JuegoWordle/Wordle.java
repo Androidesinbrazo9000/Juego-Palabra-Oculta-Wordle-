5c0b861c146ed0127a7119e63a1bd893cf7f1e2f
Androidesinbrazo9000 <estebansamuelvera2@gmail.com>
2026-05-12 08:25:51 +0200

    Pequeño desarrollo del main

---
+++ Wordle_nuevo/src/JuegoWordle/Wordle.java
@@ -0,0 +1,34 @@
+package JuegoWordle;
+import java.util.Scanner;
+public class Wordle {
+
+	public static void main (String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int opcion;
+	
+	do {
+	System.out.println("1- Cargar partida");
+	System.out.println("2- Nueva partida");
+	System.out.println("3- Consultar puntuaciones");
+	System.out.println("0- Salir");
+		opcion = sc.nextInt();
+		
+	switch(opcion) {
+	case 1:
+				
+		break;
+	case 2:
+		
+		break;
+		
+	case 3: 
+		break;
+		
+	case 0: 
+		
+		break;
+	}
+	}while(opcion != 0);	
+	}
+	
+}
