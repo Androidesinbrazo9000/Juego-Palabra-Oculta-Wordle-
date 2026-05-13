b31b0753b2e66682f98ce93851179e2842144e69
Androidesinbrazo9000 <estebansamuelvera2@gmail.com>
2026-05-13 20:49:13 +0200

    ddf

--- Wordle_nuevo/src/JuegoWordle/Partida.java
+++ Wordle_nuevo/src/JuegoWordle/Partida.java
@@ -1,9 +1,10 @@
 package JuegoWordle;
 
+import java.io.Serializable;
 import java.util.HashSet;
 
 public class Partida implements Serializable {
 	
 	private HashSet<String> palabras = new HashSet<>();
-
+punto
 }
