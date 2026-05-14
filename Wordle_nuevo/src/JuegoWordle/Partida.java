8f0f00ee09908b8069a98824661fd065bcc62e80
CristinaB93 <cberrocalm93@gmail.com>
2026-05-14 16:45:30 +0200

    He implementado el método para cargar las palabras en el hashset

--- Wordle_nuevo/src/JuegoWordle/Partida.java
+++ Wordle_nuevo/src/JuegoWordle/Partida.java
@@ -1,10 +1,46 @@
 package JuegoWordle;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.HashSet;
 
 public class Partida implements Serializable {
-	
+
+
+	private int puntuacion;
 	private HashSet<String> palabras = new HashSet<>();
-punto
+	
+	public Partida () {
+		cargarPalabras();
+	}
+
+	public void cargarPalabras () {
+		BufferedReader br = null;
+		String [] arrayPalabras;
+
+		try {
+			br = new BufferedReader(new FileReader ("Palabras.txt"));
+			String linea = br.readLine();
+			arrayPalabras = linea.split(",");
+			for (int i = 0; i < arrayPalabras.length; i++) {
+				arrayPalabras[i] = arrayPalabras[i].trim();
+				palabras.add(arrayPalabras[i]);
+			}
+		} catch (FileNotFoundException ex) {
+			System.out.println("No se han podido encontrar las palabras secretas.");
+		} catch (IOException ex) {
+			System.out.println("Error.");
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+			} catch (IOException ex) {
+				System.out.println("Error en el cierre del BR.");
+			}
+		}
+	}
+
 }
